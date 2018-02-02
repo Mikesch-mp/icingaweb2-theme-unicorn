f74fc7b7e7fa8a6bc4088cd8233ae0773d97a67e
Carsten Koebke <carsten.koebke@koebbes.de>
2018-02-02 18:05:56 +0100

    load js only if unicorn theme is active. fixes #2

--- configuration.php
+++ configuration.php
@@ -1,5 +1,28 @@
 <?php
 
-$this->provideJsFile('unicorn.js');
+use Icinga\Application\Icinga;
+use Icinga\Authentication\Auth;
 
+
+$themingConfig = Icinga::app()->getConfig()->getSection('themes');
+$defaultTheme = $themingConfig->get('default');
+
+$theme = null;
+if ($defaultTheme !== null) {
+  $theme = $defaultTheme;
+}
+
+if (! (bool) $themingConfig->get('disabled', false)) {
+  $auth = Auth::getInstance();
+  if ($auth->isAuthenticated()) {
+    $userTheme = $auth->getUser()->getPreferences()->getValue('icingaweb', 'theme');
+    if ($userTheme !== null) {
+      $theme = $userTheme;
+    }
+  }
+}
+
+if ($theme == "unicorn/unicorn") {
+  $this->provideJsFile('unicorn.js');
+}
 ?>
